bafc5d5935186690a273f6c3b144ae60d4026f79
Micheal <michealbehailu94@gmail.com>
2026-03-18 15:05:54 +0300

    Building the landing page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,16 +1,18 @@
 'use client'
 
+import { Button } from "@/components/ui/button"
+
 export default function NavBar(){
     return(
-        <div classname>
-            <div>
-                <h1>Micheal</h1>
+        <div className="flex justify-between ">
+            <div className="">
+                <h1 className="text-2xl font-bold text-[#00c4cc]">MICHEAL B.</h1>
             </div>
-            <div>
-                 <p>Home</p>
-                 <p>Projects</p>
-                 <p>Skills</p>
-                 <p>Contact</p>
+            <div className="flex justify-center gap-8">
+                 <p className='text-xl font-semibold text-[#00c4cc]'>Home</p>
+                 <p className='text-xl font-semibold text-[#00c4cc]'>Projects</p>
+                 <p className='text-xl font-semibold text-[#00c4cc]'>Skills</p>
+                 <Button className='text-xl font-semibold text-[#00c4cc] border border-[#00c4cc] bg-transparent hover:bg-[#00c4cc]/30 cursor-pointer' type="button">Contact Me</Button>
                
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
-'use client'
+"use client";
 import NavBar from "./NavBar";
 
 export default function Home() {
+  const retroStyle = {
+    backgroundImage:
+      'url("https://unsplash.com/photos/mountain-covering-with-clouds-nN1HSDtKdlw")',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "100vh",
+    filter: "grayscale(100%)",
+    color: "white",
+  };
   return (
-    <section>
+    <section
+      className="flex justify-between flex-col p-10 m-5 w-[95%]"
+      style={retroStyle}
+    >
       <NavBar />
-      Home Page
+      <div className="flex flex-col text-center items-center justify-center h-[80vh] pt-50 pb-50">
+        <h1 className="text-5xl ">Hi, I’m Micheal Behailu</h1>
+        <h2 className="text-2xl text-[#00c4cc] w-[45%] pt-2">
+          I am a senior software engineer student and I build responsive and
+          user-friendly websites and desktop applications using modern tools and
+          technologies.{" "}
+        </h2>
+      </div>
     </section>
   );
 }
