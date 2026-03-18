a210165eac49833cddf1af5cb221827deeb6327e
Micheal <michealbehailu94@gmail.com>
2026-03-18 14:42:15 +0300

    Installing shadcn

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,20 @@
 'use client'
 
-export default fumction NavBar(){
+export default function NavBar(){
     return(
-        
+        <div classname>
+            <div>
+                <h1>Micheal</h1>
+            </div>
+            <div>
+                 <p>Home</p>
+                 <p>Projects</p>
+                 <p>Skills</p>
+                 <p>Contact</p>
+               
+            </div>
+        </div>
     )
 }
+
+//button contact me // which sends the user to mailto: micheal@example.com
