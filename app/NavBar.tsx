be1ce7aa1439ae98f297bbe33b3ed640fd1e8bd5
Micheal <michealbehailu94@gmail.com>
2026-03-20 22:48:10 +0300

    Changing the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,23 +1,36 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { House, Layers, Sparkles, User, Mail } from "lucide-react";
 
-export default function NavBar(){
-    return(
-        <div className="flex justify-between ">
-            <div className="">
-                <h1 className="text-2xl font-bold">MICHEAL B.</h1>
-            </div>
-            <div className="flex justify-center gap-8">
-                 <p className='text-xl font-semibold text-[#EAC048]'>Home</p>
-                 <p className='text-xl font-semibold text-[#EAC048]'>Projects</p>
-                 <p className='text-xl font-semibold text-[#EAC048]'>Skills</p>
-                 <p className='text-xl font-semibold text-[#EAC048]'>About</p>
-                 <Button className='text-xl font-semibold text-[#EAC048] border border-[#EAC048] bg-transparent hover:bg-[#EAC048]/30 cursor-pointer' type="button">Contact Me</Button>
-               
-            </div>
-        </div>
-    )
+export default function NavBar() {
+  return (
+    <div className="flex justify-between ">
+      <div className="">
+        <h1 className="text-2xl font-bold">MICHEAL B.</h1>
+      </div>
+      <div className="flex justify-center gap-8">
+        <p className="text-xl font-semibold text-[#EAC048] flex items-center gap-2">
+          <House /> Home
+        </p>
+        <p className="text-xl font-semibold text-[#EAC048] flex items-center gap-2">
+          <Layers /> Projects
+        </p>
+        <p className="text-xl font-semibold text-[#EAC048] flex items-center gap-2">
+          <Sparkles /> Skills
+        </p>
+        <p className="text-xl font-semibold text-[#EAC048] flex items-center gap-2">
+          <User /> About
+        </p>
+        <Button
+          className="text-xl font-semibold text-[#EAC048] border border-[#EAC048] bg-transparent hover:bg-[#EAC048]/30 cursor-pointer"
+          type="button"
+        >
+          <Mail /> Contact Me
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 //button contact me // which sends the user to mailto: micheal@example.com
